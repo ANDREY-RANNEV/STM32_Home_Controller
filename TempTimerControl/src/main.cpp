8f7f5118f3e015fe77757b0b2c4ed7dbe7ba2181
ANDREY-RANNEV <66748147+ANDREY-RANNEV@users.noreply.github.com>
2022-01-31 17:26:43 +0400

    ф

--- TempTimerControl/src/main.cpp
+++ TempTimerControl/src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
+  // Включаем последовательный порт
+  Serial.begin(115200);
+  // ждем пока принициализируется USB COM порт
+  while (!Serial)
+    ;
+  Serial.println("Проект запущен");
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
